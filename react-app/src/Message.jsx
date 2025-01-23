0bd45381985f1bc90a5b7ba5b4109ecc64e4dfde
Gahalot <deepak.gahalot@revvity.com>
2025-01-23 12:58:30 +0530

    Initial commit

---
+++ react-app/src/Message.jsx
@@ -0,0 +1,14 @@
+
+
+//PascalCasing
+function Message(){
+    //JSX: Javascript XML
+    const name = ""
+    if(name)
+        return <h1>Hello{name}</h1>
+    return <h1>Hello</h1>
+    //we can write anything whioch returns a value <>{}<>
+
+}
+
+export default Message;
